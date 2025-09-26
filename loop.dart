aa002d9eb475af1effbc5f3ff55176f255aa5a9d
Mosammat Samiea Jahan Eva <samieajahaneva9543@gmail.com>
2025-09-26 07:49:20 +0600

    Create loop.dart

---
+++ loop.dart
@@ -0,0 +1,54 @@
+void main() {
+  List contactList = [
+    '01234567890',
+    '09876543210',
+    '12345678901',
+    '10987654321',
+  ];
+  //using for loop
+  for (int i = 0; i < contactList.length; i++) {
+    print(contactList[i]);
+  }
+
+  for (var i = 0; i < 10; i++) {
+    if (i == 5) {
+      continue; //it will stop the loop when i is 5.
+    } else if (i == 8) {
+      break; //it will stop the loop when i is 8.
+    }
+    print(i);
+  }
+
+  //using for in loop
+  List myList = ['A', 'B', 'C', 'D'];
+  for (var element in myList) {
+    print(element);
+  }
+
+  //for each loop
+  var myList1 = [
+    {'name': 'Name one'},
+    {'name': 'Name two'},
+    {'name': 'Name three'},
+    {'name': 'Name four'},
+  ];
+  myList.forEach((element) {
+    print(element['name']);
+  });
+
+  //while loop
+  int i = 1;
+  int value = 5;
+  while (i <= value) {
+    print(i);
+    i++;
+  }
+
+  //do while loop
+  int j = 1;
+  int value1 = 5;
+  do {
+    print(j);
+    j++;
+  } while (j <= value1);
+}

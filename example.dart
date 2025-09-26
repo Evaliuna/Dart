ccb48a75c2aa6a40b4f05e165847cfae81552c07
Mosammat Samiea Jahan Eva <samieajahaneva9543@gmail.com>
2025-09-26 15:58:23 +0600

    using oop class

---
+++ example.dart
@@ -0,0 +1,11 @@
+class Example {
+  int age = 10;
+  String name = "EVA";
+  myfunction() {
+    print("This is my function");
+  }
+
+  myfunctionTwo() {
+    print("This is my second function");
+  }
+}
---
+++ index.dart
@@ -0,0 +1,7 @@
+import 'example.dart';
+
+void main() {
+  var obj = Example();
+  print(obj.name);
+  obj.myfunction();
+}

b0feabcdbb34daa1ac1a869a4aa3e2eed8d7c5b5
Mosammat Samiea Jahan Eva <samieajahaneva9543@gmail.com>
2025-09-26 12:02:25 +0600

    Create exception.dart

---
+++ exception.dart
@@ -0,0 +1,64 @@
+void main() {
+  //exception handling using try ,on,catch ,stack-trace and finally
+
+  //format exception
+  /*int result = int.parse('44');
+  print(result.runtimeType); //it will show error to handle this error we use try catch block.
+  */
+  //try-catch block
+  try {
+    int result = int.parse('44');
+    print(result.runtimeType);
+  } catch (e) {
+    print(e);
+  }
+  //strack-trace. strack-trace is used to know that libraries breaking while creating the exception.
+  try {
+    int result = int.parse('44');
+    print(result.runtimeType);
+  } catch (e, s) {
+    print(s);
+  }
+
+  //integer division by zero exception
+  /*var result = 10~/0;
+  print(result);//it will show error to handle this error we use try catch block.
+  */
+  //on keyword used to catch specific exception.when we know the exception type then we use on keyword.
+  try {
+    var result = 10 ~/ 0;
+    print(result);
+  } on IntegerDivisionByZeroException {
+    print(
+      "cannot divide by zero",
+    ); //it will show this message when exception occurs.
+  } //when on block will execute then catch block will not execute.
+  catch (e) {
+    print(e);
+  } finally {
+    print(
+      "this is finally block",
+    ); //it will always execute whether exception occurs or not.
+  }
+}
+
+class Value implements Exception {
+  String lessThanFive() {
+    return 'Value is less than five';
+  }
+
+  String greaterThanTen() {
+    return 'Value is greater than ten';
+  }
+}
+
+value(int v) {
+  if (v < 5) {
+    throw Value()
+        .lessThanFive(); //throw keyword is used to throw exception manually.
+  } else if (v > 10) {
+    throw Value().greaterThanTen();
+  } else {
+    print('Successful');
+  }
+}

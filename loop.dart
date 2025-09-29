3e70b17eb0d82507c37a56b5676d4300fa9a6bc9
Mosammat Samiea Jahan Eva <samieajahaneva9543@gmail.com>
2025-09-29 21:00:18 +0600

    Update loop.dart

--- loop.dart
+++ loop.dart
@@ -64,4 +64,16 @@ void main() {
         "Product Name is ${Oneproduct['name']} and Price is ${Oneproduct['price']}"; //rather than using + we can use $ to take input variable in string.
     print(item);
   }
+
+  //for loop
+  int num1 = 90;
+
+  var result = num1 < 100 ? "It is less than 100" : "It is greater than 100";
+  print(result);
+
+  int? num2 = null;
+
+  var result1 =
+      num2 ?? 'It is null'; //if num2 is null then it will take 'It is null'.
+  print(result1);
 }
